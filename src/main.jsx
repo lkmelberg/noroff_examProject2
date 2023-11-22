94a6044c0f1db89838cec9867a0672be11d3f68c
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-11-22 15:58:04 +0100

    added heading and body fonts

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { theme } from "./theme.ts";
+// Supports weights 300-800
+import "@fontsource-variable/open-sans";
+// Supports weights 400-900
+import "@fontsource-variable/playfair-display";
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
