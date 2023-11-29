d1f5c12947169aec6e39198497c4703bd498e177
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-11-29 13:47:42 +0100

    added footer and updated logo image

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,66 @@
 import React from "react";
 
+import {
+  ButtonGroup,
+  Container,
+  IconButton,
+  Stack,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import logoLight from "../../assets/logo/logoLight.svg";
+
 export function Footer() {
-  return <div>This is the Footer</div>;
+  return (
+    <div>
+      {" "}
+      <Container
+        as="footer"
+        role="contentinfo"
+        py={{
+          base: "12",
+          md: "16",
+        }}>
+        <Stack
+          spacing={{
+            base: "4",
+            md: "5",
+          }}>
+          <Stack justify="space-between" direction="row" align="center">
+            <Image
+              ali
+              width={150}
+              height={50}
+              src={logoLight}
+              alt="Logo for website Holidaze"
+            />
+            <ButtonGroup variant="tertiary">
+              <IconButton
+                as="a"
+                href="#"
+                aria-label="LinkedIn"
+                icon={<FaLinkedin />}
+              />
+              <IconButton
+                as="a"
+                href="#"
+                aria-label="GitHub"
+                icon={<FaGithub />}
+              />
+              <IconButton
+                as="a"
+                href="#"
+                aria-label="Twitter"
+                icon={<FaTwitter />}
+              />
+            </ButtonGroup>
+          </Stack>
+          <Text fontSize="sm" color="fg.subtle">
+            &copy; {new Date().getFullYear()} Holidaze Inc. All rights reserved.
+          </Text>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
