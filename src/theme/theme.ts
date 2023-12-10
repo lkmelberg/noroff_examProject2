8c40e4bf72de0fa101ef62b34e08f510c5a915f7
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-12-10 22:36:00 +0100

    Final update to readme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,5 @@
-// theme.ts (Version 2 needs to be a tsx file, due to usage of StyleFunctions)
 import { extendTheme } from "@chakra-ui/react";
 
-// Version 1: Using objects
 export const theme = extendTheme({
   textStyles: {
     playfair: {
@@ -107,13 +105,6 @@ export const theme = extendTheme({
         bottom: "0",
         right: "0",
       },
-      // styles for the `a`
-      // a: {
-      //   color: "teal.500",
-      //   _hover: {
-      //     textDecoration: "underline",
-      //   },
-      // },
     },
   },
 });
